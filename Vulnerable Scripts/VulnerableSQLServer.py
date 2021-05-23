dd4596ee248302e1ff75dddddf67cc5643816590
DrSquidX <68380622+DrSquidX@users.noreply.github.com>
2021-05-23 18:58:21 +0200

    Update VulnerableSQLServer.py

--- Vulnerable Scripts/VulnerableSQLServer.py
+++ Vulnerable Scripts/VulnerableSQLServer.py
@@ -4,6 +4,9 @@ class WebServer:
     def __init__(self):
         self.logo()
         self.valid = False
+        self.name_list = ["admin adminpassword123456", "bobby cheeseburger69", "david 19216801", "mine craft", "jerry password", "tom jerry"]
+        self.names = ["admin","bobby","david","mine","jerry","tom"]
+        self.passwords = ["adminpassword123456", "cheeseburger69", "19216801", "craft", "password", "jerry"]
         try:
             self.ip = sys.argv[1]
             self.port = int(sys.argv[2])
@@ -17,13 +20,16 @@ class WebServer:
             try:
                 cursor.execute("select * from users")
             except:
-                cursor.execute("create table users(name, password)")
+                cursor.execute("create table users(name, password, logins)")
             try:
-                cursor.execute("delete from users where name = 'admin'")
+                for i in self.name_list:
+                    cursor.execute(f"delete from users where name = '{i.split()[0]}'")
             except:
                 pass
-            cursor.execute("insert into users values('admin','adminpassword123456')")
-            print("\n[+] Try to break into the 'admin' account(the password is 'adminpassword123456' if you give up)!")
+            for i in self.name_list:
+                cursor.execute(f"insert into users values('{i.split()[0]}', '{i.split()[1]}', '0')")
+            print(f"\n[+] Try to break into these accounts via SQL Injection: {self.names}")
+            print(f"[+] In case you give up, here are the passwords to the accounts: {self.passwords}\n")
             try:
                 self.externalip = sys.argv[3]
             except Exception as e:
@@ -95,12 +101,13 @@ This is a horrible looking login page. It is meant to be vulnerable to SQL Injec
         "+").replace("%3A", ":").replace("%28", "(").replace("%29", ")").replace("%2C", ","
         ).replace("%3B", ";").replace("%20", " ").replace("%3F", "?").replace("%5C", "\\"
         ).replace("%7B", "{").replace("%7D", "}").replace("%24", "$").replace("%0D", "\n"
-        ).replace("%0A", "   ").replace("%40","@")
+        ).replace("%0A", "   ").replace("%40","@").replace("%25", "%")
     def authenticate(self, query):
         db = sqlite3.connect(self.dbfile)
         cursor = db.cursor()
         cursor.execute(query)
-        if len(cursor.fetchall()) >= 1:
+        item = cursor.fetchall()
+        if len(item) >= 1:
             return True
         else:
             return False
@@ -110,8 +117,8 @@ This is a horrible looking login page. It is meant to be vulnerable to SQL Injec
             conn.send('Content-Type: text/html\n'.encode())
             if "/?name=" in msg.split()[1] and "password" in msg.split()[1]:
                 try:
-                    username = self.simplify_str(str(msg).split()[1].split("=")[1].replace("&password",""))
-                    password = self.simplify_str(str(msg).split()[1].split("=")[2])
+                    username = self.simplify_str(str(msg).split()[1].split("=")[1].replace("&password","")).lower()
+                    password = self.simplify_str(str(msg).split()[1].split("=")[2]).lower()
                     script = ""
                     if username.strip() == "" or password.strip() == "":
                         conn.send(self.packet.encode())
@@ -122,6 +129,7 @@ This is a horrible looking login page. It is meant to be vulnerable to SQL Injec
                             print(f"[+] {ip} has logged into '{username}' with '{password}'.")
                         else:
                             script = "<script>alert('Invalid Name or Password.')</script>"
+                            print(f"[+] {ip} had attempted to login to '{username}' with '{password}' but failed.")
                         packet = self.gen_packet(sqlquery=query, script=script)
                         conn.send(packet.encode())
                 except Exception as e:
